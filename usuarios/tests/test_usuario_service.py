b7a41f4465edf2d22f882b54842763e3da1fe7a4
J8A9902 <j.ochoao@uniandes.edu.co>
2023-10-15 16:45:38 -0500

    AJ-115 Se agrega prueba unitaria para campos faltantes

--- usuarios/tests/test_usuario_service.py
+++ usuarios/tests/test_usuario_service.py
@@ -24,3 +24,21 @@ async def test_create_usuario():
 
     assert test_response.status_code == HTTPStatus.CREATED
 
+@pytest.mark.asyncio
+async def test_create_usuario_faltan_campos():
+    await init()
+
+    test_response = await usuario_service.create_candidato({
+            "nombre": "Juan Jose Ochoa Ortiz",
+            "tipoDocumento": 1,
+            "documento": 1234567,
+            "username": "juanochoa",
+            "password": "password",
+            "email": "ejemplo@ejemplo.com",
+            "fechaNacimiento": 20230601,
+            "telefono": 123456,
+            "pais": "Colombia"
+        })
+
+    assert test_response.status_code == HTTPStatus.BAD_REQUEST
+
