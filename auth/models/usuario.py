7d14fae1cbf107039f1649760d99a63f382d7611
criverao <c.riverao@uniandes.edu.co>
2023-11-18 23:53:23 -0500

    Agregar pruebas unitarias al servicio de autenticación

--- auth/models/usuario.py
+++ auth/models/usuario.py
@@ -8,14 +8,18 @@ USER_ID_SEQ = Sequence('user_id_seq')
 class Usuario(BaseModel):
     id = db.Column(db.Integer, USER_ID_SEQ, primary_key=True, autoincrement=True)
     nombre = db.Column(db.String, unique=False, nullable=False)
-    tipo_documento = db.db.Column(db.Integer, unique=False, nullable=False)
+    tipo_documento = db.Column(db.Integer, unique=False, nullable=False)
     documento = db.Column(db.String, unique=True, nullable=False)
     username = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
-    rol = db.db.Column(db.Integer, unique=False, nullable=False)
+    rol = db.Column(db.Integer, unique=False, nullable=False)
 
-    def __init__(self, username: str, email: str, password: str):
+    def __init__(self, nombre: str, tipo_documento: int, documento: str, username: str, email: str, password: str, rol: int):
+        self.nombre = nombre
+        self.tipo_documento = tipo_documento
+        self.documento = documento
         self.username = username
         self.email = email
         self.password = password
+        self.rol = rol
