09097523e1540f767635bbc5ca670dfdf3c3a0f7
criverao <c.riverao@uniandes.edu.co>
2023-11-25 19:27:15 -0500

    Corrección de permisos en servicio de proyectos

--- proyectos/src/dtos/__init__.py
+++ proyectos/src/dtos/__init__.py
@@ -3,3 +3,4 @@ from .create_empleado_dto import CreateEmpleadoDto
 from .response_dto import ResponseDto
 from .exception_dto import ExceptionDto
 from .asociate_empleado_dto import AsociateEmpleadoDto
+from .get_user_dto import GetUserDto
